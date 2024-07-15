d502da07aed360f62396c28f0943478d67dfa667
Shiva04122000 <eshiva44@gmail.com>
2024-07-15 18:26:19 +0530

    Signup and SignIn UI Done

--- frontend/src/pages/SignIn/index.jsx
+++ frontend/src/pages/SignIn/index.jsx
@@ -1,12 +1,50 @@
-import React from "react";
-import { useNavigate } from "react-router-dom";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import styles from "./SignIn.module.scss";
 
 const index = () => {
-  const navigate = useNavigate();
+  const [data, setData] = useState({});
+
+  const handleOnChange = (e) => {
+    setData({ ...data, [e?.target?.name]: e?.target?.value });
+  };
+
+  const inputs = [
+    {
+      type: "email",
+      name: "email",
+      placeholder: "Email",
+    },
+    {
+      type: "text",
+      name: "password",
+      placeholder: "Password",
+    },
+  ];
+
   return (
     <main>
-      <h1>This Login Page</h1>
-      <button onClick={() => navigate("/sign-up")}>Sign Up</button>
+      <h1 className={styles.title}>Sign In</h1>
+      <form className={styles.form}>
+        {inputs.map((elem, idx) => {
+          return (
+            <input
+              key={idx}
+              placeholder={elem?.placeholder}
+              type={elem?.type}
+              name={elem?.name}
+              onChange={(e) => handleOnChange(e)}
+            />
+          );
+        })}
+        <button>Sign In</button>
+        <p className={styles.text}>
+          Don't Have an account ?{" "}
+          <Link className={styles.link} to={"/sign-up"}>
+            Sign up
+          </Link>
+        </p>
+      </form>
     </main>
   );
 };
--- frontend/src/pages/SignUp/index.jsx
+++ frontend/src/pages/SignUp/index.jsx
@@ -1,7 +1,57 @@
-import React from "react";
+import React, { useState } from "react";
+import styles from "../SignIn/SignIn.module.scss";
+import { Link } from "react-router-dom";
 
 const index = () => {
-  return <div>Register Page</div>;
+  const [data, setData] = useState({});
+
+  const handleOnChange = (e) => {
+    setData({ ...data, [e?.target?.name]: e?.target?.value });
+  };
+
+  const inputs = [
+    {
+      type: "text",
+      name: "userName",
+      placeholder: "Username",
+    },
+    {
+      type: "email",
+      name: "email",
+      placeholder: "Email",
+    },
+    {
+      type: "text",
+      name: "password",
+      placeholder: "Password",
+    },
+  ];
+
+  return (
+    <main>
+      <h1 className={styles.title}>Sign Up</h1>
+      <form className={styles.form}>
+        {inputs.map((elem, idx) => {
+          return (
+            <input
+              key={idx}
+              placeholder={elem?.placeholder}
+              type={elem?.type}
+              name={elem?.name}
+              onChange={(e) => handleOnChange(e)}
+            />
+          );
+        })}
+        <button>Sign Up</button>
+        <p className={styles.text}>
+          Have an account ?{" "}
+          <Link className={styles.link} to={"/sign-in"}>
+            Sign In
+          </Link>
+        </p>
+      </form>
+    </main>
+  );
 };
 
 export default index;
