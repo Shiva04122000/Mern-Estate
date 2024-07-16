c81dd335148b9fe12ae0637e52634d32a2420e92
Shiva04122000 <eshiva44@gmail.com>
2024-07-16 14:22:40 +0530

    validation added for signup in backend

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,9 +1,17 @@
 import User from "../models/user.model.js";
 import bcrypt from "bcryptjs";
+import errorHandler from "../utils/error.js";
 
 export const signup = async (req, res, next) => {
   try {
     const { userName, password, email } = req.body;
+    if ([userName, password, email].some((field) => field.trim() === "")) {
+      return next(new errorHandler(400, "Some fields are missing"));
+    }
+
+    const userExists = await User.findOne({ email });
+    if (userExists) return next(new errorHandler(400, "User Already Exists"));
+
     const hashedPassword = await bcrypt.hash(password, 10);
     const user = new User({ userName, password: hashedPassword, email });
     await user.save();
