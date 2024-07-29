5f9c1dd75d07d3bd33643299e4227a6136b0e388
Shiva04122000 <eshiva44@gmail.com>
2024-07-29 14:59:36 +0530

    Added Redux Persist

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,11 +5,14 @@ import { RouterProvider } from "react-router-dom";
 import router from "./router";
 import { Toaster } from "react-hot-toast";
 import { Provider } from "react-redux";
-import { store } from "./redux/store";
+import { persistedStore, store } from "./redux/store";
+import { PersistGate } from "redux-persist/integration/react";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
-    <Toaster position={"top-right"} />
-    <RouterProvider router={router} />
+    <PersistGate loading={null} persistor={persistedStore}>
+      <Toaster position={"top-right"} />
+      <RouterProvider router={router} />
+    </PersistGate>
   </Provider>
 );
--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -1,8 +1,20 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import userReducer from "./reducers/userSlice";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
+const rootReducers = combineReducers({ user: userReducer });
+
+const persisitConfig = {
+  key: "root",
+  storage,
+  version: 1,
+};
+
+const persisitedReducer = persistReducer(persisitConfig, rootReducers);
 
 export const store = configureStore({
-  reducer: {
-    user: userReducer,
-  },
+  reducer: persisitedReducer,
 });
+
+export const persistedStore = persistStore(store);
