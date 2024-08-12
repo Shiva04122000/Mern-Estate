ce03608882ab4db9b355ed9caff6a85a18e4558a
Shiva04122000 <eshiva44@gmail.com>
2024-08-12 17:03:15 +0530

    build changes added

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ import express, { json } from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 import cookieParser from "cookie-parser";
+import path from "path";
 
 import connectDb from "./data/dbConnect.js";
 import authRouter from "./routes/auth.route.js";
@@ -9,6 +10,7 @@ import userRouter from "./routes/user.route.js";
 import listingRouter from "./routes/listing.route.js";
 import { errorMiddleware } from "./utils/error.js";
 
+const __dirname = path.resolve();
 const app = express();
 const PORT = 3000;
 
@@ -33,5 +35,11 @@ app.use(express.json());
 app.use("/api/auth", authRouter);
 app.use("/api/user", userRouter);
 app.use("/api/listing", listingRouter);
+
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
 
 app.use(errorMiddleware);
