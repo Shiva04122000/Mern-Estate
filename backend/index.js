a1ab84b742b6f98815aea63a4bf3475b5db8cb86
Shiva04122000 <eshiva44@gmail.com>
2024-08-12 17:27:20 +0530

    build Changes made

--- backend/index.js
+++ backend/index.js
@@ -36,10 +36,10 @@ app.use("/api/auth", authRouter);
 app.use("/api/user", userRouter);
 app.use("/api/listing", listingRouter);
 
-app.use(express.static(path.join(__dirname, "/frontend/dist")));
+app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+  res.sendFile(path.join(__dirname, "../frontend/dist", "index.html"));
 });
 
 app.use(errorMiddleware);
